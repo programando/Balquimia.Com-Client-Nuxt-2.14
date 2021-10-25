74d488eb2d0abe11afe876c3031d002afb16f72c
JeisonPC <85714780+JeisonPC@users.noreply.github.com>
2021-10-25 10:42:54 -0500

    Update chat.vue
    Actualización texto mensaje Whatsapp.

--- components/home/whatsapp/chat.vue
+++ components/home/whatsapp/chat.vue
@@ -1,10 +1,11 @@
 <template>
 
 <div v-b-hover="chat" class="scale-50 mr-0 z-50 bottom-0 right-0 fixed">
-    <a href="https://wa.me/573174322752/?text=Hola.%20Necesito%20asesoría%20personalizada sobre productos industriales...." 
+    <a href="https://wa.me/573174322752/?text=Hola.%20Necesito%20asesoría%20personalizada%20sobre%20productos%20industriales%20Balquimia....." 
     target="_blank"><img id="" class="w-28 mr-8 mb-8 hover:scale-100 ..." :src="require('/static/whatsapp.png')" alt=""/></a>
 
 </div>
+
 
 </template>
 
