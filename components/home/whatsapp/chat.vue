7ec68f21e5acdf1acdab5b91512f73610a80e8c1
JeisonPC <85714780+JeisonPC@users.noreply.github.com>
2021-10-25 10:31:56 -0500

    Titulo mensaje
    Amplié titulo mensaje.  Quie debe ser muy general... no necesitamos tanto detalle puesto que a veces hacemos cambios en muchos archivos

---
+++ components/home/whatsapp/chat.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div v-b-hover="chat" class="scale-50 mr-0 z-50 bottom-0 right-0 fixed">
+    <a href="https://wa.me/573174322752/?text=Hola.%20Necesito%20asesoría%20personalizada sobre productos industriales...." 
+    target="_blank"><img id="" class="w-28 mr-8 mb-8 hover:scale-100 ..." :src="require('/static/whatsapp.png')" alt=""/></a>
+
+</div>
+
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+
+#chatcito:hover{
+    width: 30%;
+}
+
+</style>
