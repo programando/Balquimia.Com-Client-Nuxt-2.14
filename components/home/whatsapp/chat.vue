3f740034e5265e651f1efec70ccf96c78e61a9b4
JeisonPC <85714780+JeisonPC@users.noreply.github.com>
2021-10-25 10:50:57 -0500

    imagen whatsapp

--- components/home/whatsapp/chat.vue
+++ components/home/whatsapp/chat.vue
@@ -2,10 +2,9 @@
 
 <div v-b-hover="chat" class="scale-50 mr-0 z-50 bottom-0 right-0 fixed">
     <a href="https://wa.me/573174322752/?text=Hola.%20Necesito%20asesoría%20personalizada%20sobre%20productos%20industriales%20Balquimia....." 
-    target="_blank"><img id="" class="w-28 mr-8 mb-8 hover:scale-100 ..." :src="require('/static/whatsapp.png')" alt=""/></a>
+    target="_blank"><img id="" class="w-22 mr-8 mb-8 hover:scale-100 ..." :src="require('/static/whatsapp.png')" alt=""/></a>
 
 </div>
-
 
 </template>
 
